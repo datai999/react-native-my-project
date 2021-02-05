09395fee2393252fe30039a1c409c02effcb7685
Đức Anh Tài <n-tai-cna>
2021-02-05 17:17:00 +0700

    props mutli greeting

---
+++ components/MultiGreeting.js
@@ -0,0 +1,29 @@
+/*
+Nguyen Duc Anh tai 
+https://www.facebook.com/datai99/
+Email:datai28599@gmail.com
+*/
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+type GreetingProps = {  name: string };
+class Greeting extends Component<GreetingProps>{
+    render(){
+        return (
+        <Text>Hello  {this.props.name} </Text>
+        );
+    }
+}
+
+export default class MultiGreeting extends Component{
+    render(){
+        return(
+            <View
+                style = {{ alignItems: 'center'}}
+            >
+            <Greeting name="Nguyen Duc Anh Tai"></Greeting>
+            <Greeting name="Nguyen Duc Anh Tai 2nd"></Greeting>
+            </View>
+        );
+    }
+}
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 import HelloWord from './components/HelloWord';
 import MyImage from './components/MyImage';
+import MultiGreeting from './components/MultiGreeting'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(HelloWord);
+registerRootComponent(MultiGreeting);
