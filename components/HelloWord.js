5b86384ffb835855ed06d557c680276986b682df
Đức Anh Tài <n-tai-cna>
2021-02-05 16:22:40 +0700

    hello word

---
+++ components/HelloWord.js
@@ -0,0 +1,15 @@
+/*
+Nguyen Duc Anh tai 
+https://www.facebook.com/datai99/
+Email:datai28599@gmail.com
+*/
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+export default class HelloWord extends Component{
+    render() {
+        return (
+            <Text>HelloWord. My name is Tai.</Text>
+        );
+    }
+}
