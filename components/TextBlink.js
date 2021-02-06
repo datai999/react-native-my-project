84d8af097b48b1fedd05248f680f13e6a693a337
Đức Anh Tài <n-tai-cna>
2021-02-06 11:23:09 +0700

    state and blinkText

---
+++ components/TextBlink.js
@@ -0,0 +1,46 @@
+/*
+Nguyen Duc Anh tai 
+https://www.facebook.com/datai99/
+Email:datai28599@gmail.com
+*/
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+interface IBlinkProps { inputText: string }
+interface IBlinkState { showText: boolean }
+
+class Blink extends Component<IBlinkProps, IBlinkState> {
+
+  constructor(props: IBlinkProps) {
+    super(props);
+    this.state = { showText: true };
+
+    var taskToDo = () => {
+      this.setState({
+        showText: !this.state.showText
+      });
+    };
+
+    const timeToBlink = 500;
+    setInterval(taskToDo, timeToBlink);
+  }
+
+  render() {
+    let textToDisplay = this.state.showText ? this.props.inputText : 'hidden';
+    return (
+      <Text style={{ padding: 150 }}>
+        {textToDisplay}
+      </Text>
+    );
+  }
+}
+
+export default class TextBlink extends Component {
+  render() {
+    return (
+      <View>
+        <Blink inputText='Blink text ok'></Blink>
+      </View>
+    )
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import { registerRootComponent } from "expo";
 
 import App from "./App";
+
+// lesson from https://www.youtube.com/watch?v=kNHuLOXR5T0&list=PLWBrqglnjNl31S91FFE63DtuRc9v9LSGl&index=1&ab_channel=NguyenDucHoang
+//lesson 1-5
 import HelloWord from "./components/HelloWord";
-import MyImage from "./components/MyImage";
+import MyImage from "./components/MyImage"; 
 import MultiGreeting from "./components/MultiGreeting";
+import TextBlink from "./components/TextBlink";
+//lesson 6-10
+//...
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(MultiGreeting);
+registerRootComponent(TextBlink);
