c078eee3f1d74c75754f66fb0136d5e0fbaa3c11
Đức Anh Tài <n-tai-cna>
2021-02-07 12:32:06 +0700

    style widh + heigh

--- components/StyleSheet.js
+++ components/StyleSheet.js
@@ -9,16 +9,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class MyStyleSheet extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.firstText}>First Line</Text>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 20, backgroundColor: 'crimson' }}>
+          <View style={styles.container}>
+            <Text style={styles.firstText}>First Line</Text>
+          </View>
+        </View>
+        <View style={{ flex: 20, backgroundColor: 'blue' }}></View>
+        {/* first render */}
+        <View style={{ height: 123, backgroundColor: 'red' }}>
+          <View style={{ flex: 1, flexDirection: 'row', }}>
+            <View style={{ width: 20, backgroundColor: 'green' }}></View>
+            <View style={{ width: 20, backgroundColor: 'yellow' }}></View>
+          </View>
+        </View>
       </View>
+
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    margin: 50,
+    // margin: 50,
   },
   firstText: {
     color: 'red'
